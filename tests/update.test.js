101ef7b39f92f93ae1681d05b39123ec5eec7722
evansiroky <evan.siroky@yahoo.com>
2016-03-30 11:28:54 -0700

    shapefile downloads and gets extracted to GeoJSON

--- tests/update.test.js
+++ tests/update.test.js
@@ -8,7 +8,9 @@ describe('data update', function() {
   
   it('tz geojson should get updated after fetching latest tz_world shapefile', function(done) {
 
-    this.timeout(30000)
+    this.timeout(180000)
+
+    var beginTime = (new Date()).getTime()
 
     // update timezone data by downloading it and extracting to geojson
     geoTz.updateData(function(err) {
@@ -19,6 +21,9 @@ describe('data update', function() {
       fs.stat('./data/tzgeo.json', function(err, stats) {
 
         assert.isNotOk(err)
+        assert.isAbove(stats.ctime.getTime(), beginTime, 'file update time is before test!')
+
+        done()
 
       })
     })
