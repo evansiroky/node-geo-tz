8eabf1ab57d8c4943ace6dbf2b360e0da2fd49bc
evansiroky <evan.siroky@yahoo.com>
2016-05-06 00:30:42 -0700

    indexing in progress

--- tests/geoIndex.test.js
+++ tests/geoIndex.test.js
@@ -1,11 +1,30 @@
 var assert = require('chai').assert
 
-var createGeoIndex = require('./lib/createGeoIndex.js')
+var fs = require('fs-extra'),
+  rimraf = require('rimraf')
 
-var testTzData = require('./tests/data/largeTz.json'),
-  expectedIndexData = require('./tests/data/expectedIndexData.json')
+var createGeoIndex = require('../lib/createGeoIndex.js')
+
+var testTzData = require('./data/largeTz.json'),
+  expectedIndexData = require('./data/expectedIndexData.json')
 
 describe('geoindex', function() {
+
+  before(function(done) {
+    fs.rename('./data', './data-master', done)
+  })
+
+  after(function(done) {
+    fs.rename('./data-master', './data', done)
+  })
+
+  beforeEach(function(done) {
+    fs.mkdir('./data', done)
+  })
+
+  afterEach(function(done) {
+    rimraf('./data', done)
+  })
 
   it('should create geoindex of simple geometry', function(done) {
 
@@ -13,6 +32,10 @@ describe('geoindex', function() {
       function(err) {
 
         assert.isNotOk(err)
+
+        var generatedIndex = require('../data/index.json')
+
+        assert.deepEqual(generatedIndex, expectedIndexData)
 
         done()
       }) 
