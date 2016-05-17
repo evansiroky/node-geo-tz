fdaf32ebd68a79b10b598081ea5453fbc1f80853
evansiroky <evan.siroky@yahoo.com>
2016-05-17 10:50:16 -0700

    feat(geo-index): Refactor library to use quadtree index for geographic lookup of tz data
    This refactor aims to improve the speed and lower the memory usage of this library.  Before, all of
    
    the world's tz data was loaded into memory and then every single tz was searched until there was a
    
    match.  This refactor includes automatic generation of a quadtree index of the tz data although
    
    still searches in much smaller files to determine the exact timezone if needed.

--- scripts/updateData.js
+++ scripts/updateData.js
@@ -1,6 +1,6 @@
 var update = require('../lib/update.js')
 
-update({ indexGeoJSON: true }, function(err) {
+update(function(err) {
   if(err) {
     console.log('update unsuccessful.  Error: ', err)
   } else {
