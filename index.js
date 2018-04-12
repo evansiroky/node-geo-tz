1911d9cd14fef6e176e543bdc133d7fbf9facf66
evansiroky <evan.siroky@yahoo.com>
2018-04-12 15:28:56 -0700

    perf(find): refactor getTimezone function
    Simplify API to have only 1 method.  Also implement automatic caching
    of detailed lookup areas.
    
    Fixes #73
    Fixes #75
    
    BREAKING CHANGE:
    
    Change API to only have one lookup method of the name of the timezone.
    Remove dependency on moment-timezone.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 var find = require('./lib/find.js')
 
-module.exports = {
-  tz: find.timezone,
-  tzMoment: find.timezoneMoment,
-  createPreloadedFeatureProvider: find.createPreloadedFeatureProvider
-}
+module.exports = find
