7ad73333b172f78601bea5b2d772e94c4d3c446c
Misha Nasledov <788577+mishan@users.noreply.github.com>
2021-11-08 14:06:08 -0800

    fix: use flat file format instead of directory tree
    - This reduces the data size from 245MB to 25MB
    - Removes data.zip entirely per discussion

--- tests/geo-index.test.js
+++ tests/geo-index.test.js
@@ -10,15 +10,26 @@ var util = require('./util.js')
 var createGeoIndex = require('../lib/geo-index')
 
 var TEST_DATA_DIR = __dirname + '/../data-test-geoindex'
+var TEST_GEO_DAT = `${TEST_DATA_DIR}/geo.dat`
+var TEST_INDEX_FILE = `${TEST_DATA_DIR}/index.json`
 var testTzData = require('./fixtures/largeTz.json')
 var expectedIndexData = require('./fixtures/expectedIndexData.json')
 
 /**
  * Synchronously extracts data from the given subzoneFile and verifies that it
  * matches the passed expectedData.
+ *
+ * @param {number} pos
+ * @param {number} len
+ * @param {object} expectedData
+ * @returns {void}
  */
-function assertSubzoneDataIsEqual (subzoneFile, expectedData) {
-  const data = new Pbf(fs.readFileSync(subzoneFile))
+function assertSubzoneDataIsEqual (pos, len, expectedData) {
+  const fd = fs.openSync(TEST_GEO_DAT, 'r')
+  const buf = Buffer.alloc(len)
+  fs.readSync(fd, buf, 0, len, pos)
+  fs.closeSync(fd)
+  const data = new Pbf(buf)
   assert.deepEqual(geobuf.decode(data), expectedData)
 }
 
@@ -39,17 +50,22 @@ describe('geoindex', function () {
       function (err) {
         assert.isNotOk(err)
 
-        var generatedIndex = require(TEST_DATA_DIR + '/index.json')
+        var generatedIndex = require(TEST_INDEX_FILE)
 
         assert.deepEqual(generatedIndex, expectedIndexData)
 
+        const zone1 = generatedIndex.lookup['b']['b']['d']['c']['d']['d']
+
         // also make sure certain subzone data is written
         assertSubzoneDataIsEqual(
-          TEST_DATA_DIR + '/b/b/d/c/d/d/geo.buf',
+          zone1.pos, zone1.len,
           require('./fixtures/expectedSubzone1.json')
         )
+
+        const zone2 = generatedIndex.lookup['b']['c']['a']['a']['a']['d']
+
         assertSubzoneDataIsEqual(
-          TEST_DATA_DIR + '/b/c/a/a/a/d/geo.buf',
+          zone2.pos, zone2.len,
           require('./fixtures/expectedSubzone2.json')
         )
 
